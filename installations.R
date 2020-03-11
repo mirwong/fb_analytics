50c6b7d9cc6484ff217a2713df4f1168e1a02b1c
Mirielle Wong <mirwong@nus12388-11-mirwong.nueva.local>
2020-03-11 11:15:41 -0700

    adding installs

---
+++ installations.R
@@ -0,0 +1,9 @@
+install.packages("rmarkdown")
+
+# Must install the devtools package using the below commented out code
+install.packages("devtools")
+
+# Then can install using the devtools package from either of the following:
+devtools::install_github(repo = "maksimhorowitz/nflscrapR")
+# or the following (these are the exact same packages):
+devtools::install_github(repo = "ryurko/nflscrapR")
